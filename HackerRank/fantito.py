165fa4b447270a8649b3576a1ca09a63f66fa5b6
NeiderFajardo <neafajardoc@correo.udistrital.edu.co>
2020-06-21 19:50:18 -0500

    Is Fibo

--- HackerRank/fantito.py
+++ HackerRank/fantito.py
@@ -1,14 +1,27 @@
 #!/bin/python3
 
+import os
+import sys
 
-def maxad(lista):
-    maximum = float('-inf')
-    for element, adjacent in zip(lista[:-1], lista[1:]):
-        candidate = element *adjacent
-        if candidate > maximum:
-            maximum =candidate
-    return maximum
+# Complete the solve function below.
+def solve(n):
+    res = 0
+    nu = 1
+    aux = 0
+    while res <= n:
+        if res == n:
+            return "IsFibo"
+        aux = res
+        res += nu
+        nu = aux
+    return "IsNotFibo"
 
-d = [3,6,-2,-5,7,3]
-c = [1]
-print(maxad(c))
+if __name__ == '__main__':
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        result = solve(n)
+
+        print(result)
